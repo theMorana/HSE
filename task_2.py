ac5fc6971b8f902ad100c268cda65c7bdd3f1a9a
MorAna <m_shtosel@mail.ru>
2025-09-29 19:15:31 +0300

    Task 2. 29.09

---
+++ task_2.py
@@ -0,0 +1,10 @@
+def main(greet_text):
+    # Функция print принимает на вход содержание переменной greet_text
+    print(greet_text)
+
+if __name__ == "__main__":
+    # 2. Допишите строчку кода: 
+    # Здесь должно производиться присваивание переменной
+    # Мы должны занести в greet_text текст приветствия вместо None
+    greet_text = "Hello, world!"
+    main(greet_text)
