6ec094b3d8a558f9b3cf125fd6331beb3e58f391
MorAna <m_shtosel@mail.ru>
2025-09-29 19:18:22 +0300

    Task 2. 29.09

---
+++ task_2.py
@@ -0,0 +1,8 @@
+def main(greet_text):
+    # Функция print принимает на вход содержание переменной greet_text
+    print(greet_text)
+
+if __name__ == "__main__":
+    
+    greet_text = "Hello, world!"
+    main(greet_text)
